93255bb9975da0ecee9cc8c20745ba46762bf087
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-01-20 21:43:14 +0100

    Better codestyle + const values

--- src/main/kotlin/wtf/mizu/plugin/MizuPlugin.kt
+++ src/main/kotlin/wtf/mizu/plugin/MizuPlugin.kt
@@ -2,45 +2,52 @@ package wtf.mizu.plugin
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.publish.PublishingExtension
 import wtf.mizu.plugin.dependency.dependsOn
 import wtf.mizu.plugin.dependency.repository
 import wtf.mizu.plugin.extension.MizuExtension
 import wtf.mizu.plugin.publishing.mizuPublishMavenRepository
-import java.net.URI
 
 class MizuPlugin: Plugin<Project> {
+    companion object {
+        internal const val NAME = "mizu"
+        internal const val GROUP_ID = "wtf.$NAME"
 
-    override fun apply(project: Project) {
-        val extension = project.extensions.create("mizu", MizuExtension::class.java)
+        internal const val REPO_URL = "https://maven.mizu.wtf/"
+        internal const val REPO_RELEASES_URL = "$REPO_URL/releases"
+        internal const val REPO_SNAPSHOTS_URL = "$REPO_URL/snapshots"
+        internal const val REPO_PRIVATE_URL = "$REPO_URL/private"
+    }
 
-        project.afterEvaluate { a ->
+    override fun apply(target: Project) {
+        val extension = target.extensions.create(NAME, MizuExtension::class.java)
+
+        target.afterEvaluate { project ->
             // Adds the repositories of mizu
-            project.repository("mizu-releases", "https://maven.mizu.wtf/releases")
-            project.repository("mizu-snapshots", "https://maven.mizu.wtf/snapshots")
+            project.repository("$NAME-releases", REPO_RELEASES_URL)
+            project.repository("$NAME-snapshots", REPO_SNAPSHOTS_URL)
 
             if(project.properties.containsKey("MIZU_USERNAME") && project.properties.containsKey("MIZU_TOKEN")) {
                 val username = project.properties["MIZU_USERNAME"] as String
                 val token = project.properties["MIZU_TOKEN"] as String
                 project.repository(
-                    "mizu-releases", "https://maven.mizu.wtf/private",
+                    "mizu-releases", REPO_PRIVATE_URL,
                     username, token
                 )
             }
 
             // Adds the libraries of mizu if requested
             if(extension.common.isNotEmpty())
-                project.dependsOn("wtf.mizu", "common", extension.common)
+                project.dependsOn(GROUP_ID, "common", extension.common)
             if(extension.animations.isNotEmpty())
-                project.dependsOn("wtf.mizu", "animations", extension.animations)
+                project.dependsOn(GROUP_ID, "animations", extension.animations)
             if(extension.events.isNotEmpty()) {
-                project.dependsOn("wtf.mizu", "events-api", extension.events)
-                project.dependsOn("wtf.mizu", "events-${extension.eventsImpl.id}", extension.events)
+                project.dependsOn(GROUP_ID, "events-api", extension.events)
+                project.dependsOn(GROUP_ID, "events-${extension.eventsImpl.id}", extension.events)
             }
             if(extension.settings.isNotEmpty())
-                project.dependsOn("wtf.mizu", "settings", extension.settings)
+                project.dependsOn(GROUP_ID, "settings", extension.settings)
 
-            // Adds the repositories of mizu for maven publish if its available.
+            // Adds the repositories of mizu for maven publish if it's available.
             project.mizuPublishMavenRepository()
         }
     }
