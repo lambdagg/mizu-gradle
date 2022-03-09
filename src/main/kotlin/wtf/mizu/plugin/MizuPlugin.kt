8c115fbb7bcaee61313f1d2ec1f0cb258fa7d20c
xtrm-en <26600206+xtrm-en@users.noreply.github.com>
2022-03-09 15:09:48 +0100

    main: don't depend on event-api as the implementation already scopes it as API

--- src/main/kotlin/wtf/mizu/plugin/MizuPlugin.kt
+++ src/main/kotlin/wtf/mizu/plugin/MizuPlugin.kt
@@ -7,12 +7,12 @@ import wtf.mizu.plugin.dependency.repository
 import wtf.mizu.plugin.extension.MizuExtension
 import wtf.mizu.plugin.publishing.mizuPublishMavenRepository
 
-class MizuPlugin: Plugin<Project> {
+class MizuPlugin : Plugin<Project> {
     companion object {
         internal const val NAME = "mizu"
         internal const val GROUP_ID = "wtf.$NAME"
 
-        internal const val REPO_URL = "https://maven.mizu.wtf/"
+        private const val REPO_URL = "https://maven.mizu.wtf/"
         internal const val REPO_RELEASES_URL = "$REPO_URL/releases"
         internal const val REPO_SNAPSHOTS_URL = "$REPO_URL/snapshots"
         internal const val REPO_PRIVATE_URL = "$REPO_URL/private"
@@ -26,7 +26,7 @@ class MizuPlugin: Plugin<Project> {
             project.repository("$NAME-releases", REPO_RELEASES_URL)
             project.repository("$NAME-snapshots", REPO_SNAPSHOTS_URL)
 
-            if(project.properties.containsKey("MIZU_USERNAME") && project.properties.containsKey("MIZU_TOKEN")) {
+            if (project.properties.containsKey("MIZU_USERNAME") && project.properties.containsKey("MIZU_TOKEN")) {
                 val username = project.properties["MIZU_USERNAME"] as String
                 val token = project.properties["MIZU_TOKEN"] as String
                 project.repository(
@@ -36,20 +36,24 @@ class MizuPlugin: Plugin<Project> {
             }
 
             // Adds Mizu's required libraries
-            if(extension.common.isNotEmpty())
+            if (extension.common.isNotEmpty()) {
                 project.dependsOn(GROUP_ID, "common", extension.common)
-            if(extension.animations.isNotEmpty())
+            }
+            if (extension.animations.isNotEmpty()) {
                 project.dependsOn(GROUP_ID, "animations", extension.animations)
-            if(extension.events.isNotEmpty()) {
-                project.dependsOn(GROUP_ID, "events-api", extension.events)
+            }
+            if (extension.events.isNotEmpty()) {
                 project.dependsOn(GROUP_ID, "events-${extension.eventsImpl.id}", extension.events)
             }
-            if(extension.settings.isNotEmpty())
+            if (extension.settings.isNotEmpty()) {
                 project.dependsOn(GROUP_ID, "settings", extension.settings)
-            if(extension.clientApi.isNotEmpty())
+            }
+            if (extension.clientApi.isNotEmpty()) {
                 project.dependsOn(GROUP_ID, "client-api", extension.clientApi)
-            if(extension.loader.isNotEmpty())
+            }
+            if (extension.loader.isNotEmpty()) {
                 project.dependsOn(GROUP_ID, "loader", extension.loader)
+            }
 
             // If maven-publish is available, add Mizu's repositories
             project.mizuPublishMavenRepository()
